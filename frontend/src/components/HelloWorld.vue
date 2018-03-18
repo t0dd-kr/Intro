fbcc8f52a62d795b798f76a72db24de2c35d5273
t0dd <swjasontodd@gmail.com>
2018-03-19 01:15:34 +0900

    initial backend setting Add Login Component for test Add SIngleComponent for test

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <img src="../assets/logo.png">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -88,7 +89,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Hello Todd.'
     }
   }
 }
