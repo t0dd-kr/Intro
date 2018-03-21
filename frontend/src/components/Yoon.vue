d00083bf2de101173e7416024b4ec3dd5718fc20
DESKTOP-KLP1POH\yoon <ahnsh03@gmail.com>
2018-03-21 18:53:24 +0900

    Edit Yoon change container for pratice

--- frontend/src/components/Yoon.vue
+++ frontend/src/components/Yoon.vue
@@ -1,86 +1,71 @@
 <template lang="html">
-  <div class="container-login">
+  <div class="container">
     <div class="yoon">
-      <div class="container-id">
-        <input class="email" type="email" v-model="email" placeholder="E-mail">
+      <div class="container-high">
+        <div class="container-question row">
+          <div class="high-left-question">
+            Q.
+          </div>
+          <div class="high-center-question">
+          What's your e-mail and password?
+        </div>
       </div>
-      <div class="container-pw">
-        <input type="password" v-model="password" placeholder="Password">
+      <div class="container-middle">
+        <div class="mid-left-answer">
+          A.
+        </div>
+        <div class="mid-center-item">
+          <div class="col">
+            <div class="answer">
+              <input type="email" name="email" placeholder="e-mail">
+            </div>
+            <div class="answer">
+              <input type="password" name="password" placeholder="password">
+            </div>
+            <div class="answer">
+              <button type="button"> sign in</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="container-pw-confirm">
-        <input type="password" v-model="password_confirm" placeholder="CONFIRM Password">
+      <div class="container-low">
+        <div class="error-notice low">
+          0 error occured
+        </div>
       </div>
-      <div class="container-button">
-        <button type="button" v-on:click="login">Login</button>
+      </div>
+      <div class="container-low">
+      </div>
       </div>
     </div>
-</div>
+  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'Yoon',
-  data () {
+  name: 'yoon',
+  data() {
     return {
-      email_: 'ahnsh09@naver.com',
-      password: '1111',
-      password_: '1111',
-      password_confirm: '1111',
-      email: 'ahnsh09@naver.com'
-    }
-  },
-  // computed: {
-  //   error: function () {
-  //     if (this.password === this.password_confirm) {
-  //       return 'Password Correct'
-  //     } else {
-  //       return 'Password Wrong!!'
-  //     }
-  //   },
-  //   verifyEmail: function () {
-  //     var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
-  //     if (this.email.match(regExp) !== null) {
-  //       return 'good'
-  //     } else {
-  //       return 'error'
-  //     }
-  //   }
-  // },
-  methods: {
-    login: function () {
-      if (this.email === this.email_ && this.password === this.password_ === this.password_confirm) {
-        alert('Login success')
-      } else {
-        alert('Login Fail')
-      }
+      email: 'ahnsh09@naver.com',
+      password: '1111'
     }
   }
 }
 </script>
 
 <style scoped>
-  .question-page {
-
+  .container {
+    z-index: 1;
+    display: black;
+    width: 90%;
+    color: #000;
   }
-  .container-login {
+  .container-high {
     margin: 0;
+    background-color: #e0e2e3;
     padding: .75rem;
     max-width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    box-sizing: border-box;
-
-  }
-  .container-id, .container-pw, .container-pw-confirm, .container-button {
-    margin: 0;
-    background-color: #dcd3d3;
-    padding: .75rem;
-    max-width: 100%;
-    box-sizing: border-box;
-    display: flex;
-    flex-wrap: wrap;
-  }
-  .container-id input, .container-pw input, .container-pw-confirm input, .container-button button, .errormessage{
-    padding: .5rem 1rem .5rem 1rem;
+    border-radius: .4rem .4rem 0 0;
   }
 </style>
