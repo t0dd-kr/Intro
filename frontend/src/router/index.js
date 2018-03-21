216f01774bc44e8385f6c31e5b8e520bd4a5a602
chanki <>
2018-03-21 20:20:39 +0900

    chanki first commit test(erase yoon.vue - error)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import Login from '@/components/Login'
-import Yoon from '@/components/Yoon'
 Vue.use(Router)
 
 export default new Router({
@@ -17,11 +16,6 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/Yoon',
-      name: 'Yoon',
-      component: Yoon
     }
   ]
 })
