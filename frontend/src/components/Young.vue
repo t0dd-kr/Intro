f26cde4fddcacdf3f1af62ba0bb145138332e76f
Zelord.Kwoun <>
2018-03-25 14:46:58 +0900

    fixed Young.vue error code

--- frontend/src/components/Young.vue
+++ frontend/src/components/Young.vue
@@ -1,5 +1,24 @@
-<template lang="">
+<template lang="html">
+  <div class="window_tile">
+    <div class="tile1">
 
+    </div>
+    <div class="tile2">
+
+    </div>
+    <div class="tile3">
+
+    </div>
+    <div class="tile4">
+
+    </div>
+    <div class="tile5">
+
+    </div>
+    <div class="tile6">
+
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,5 +26,25 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style scoped>
+
+  .window_tile{
+    display: -webkit-flex;
+    display: flex;
+    -webkit-flex-flow: row wrap;
+    flex-flow: row wrap;
+  }
+  .window_tile div {
+    box-sizing: border-box;
+    min-height: 150px;
+    min-width: 150px;
+  }
+  .tile1, .tile2, .tile3, .tile4, .tile5, .tile6 {
+    width: 100%;
+  }
+
+  .tile1 {
+    background-color : #fbc2eb;
+  }
+
 </style>
