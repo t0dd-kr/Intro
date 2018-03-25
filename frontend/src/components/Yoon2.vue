a05b869df1d6a8ad18d179ba99966a2f5f86c293
DESKTOP-KLP1POH\yoon <ahnsh03@gmail.com>
2018-03-25 15:21:52 +0900

    create Yoon2 Css for practice

---
+++ frontend/src/components/Yoon2.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+  <div class="ex">
+    <div class="ex1">
+    </div>
+    <div class="ex2">
+    </div>
+    <div class="ex3">
+    </div>
+    <div class="ex4">
+    </div>
+    <div class="ex5">
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+  .ex{
+    display: -webkit-flex;
+    display: flex;
+    -webkit-flex-flow: row wrap;
+    flex-flow: row wrap;
+  }
+  .ex div {
+    box-sizing: border-box;
+    min-height: 150px;
+    min-width: 150px;
+  }
+  .ex1, .ex2, .ex3, .ex4, .ex5, .ex6{
+    width: 100%;
+  }
+  .ex1 {
+    background-color : #1d4586;
+  }
+  .ex2 {
+    background-color: #3c77d9;
+  }
+  .ex3 {
+    background-color: #cadaf8;
+  }
+  .ex4 {
+    background-color: #d6dff0;
+  }
+  .ex5 {
+    background-color: #dfe1e6;
+  }
+  .ex6 {
+    background-color: ;
+  }
+</style>
