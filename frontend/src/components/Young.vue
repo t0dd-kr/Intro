f08cb342f291565d9617e3cdce070d9eff3e3523
Zelord.Kwoun <>
2018-03-27 17:23:10 +0900

    add sidebar for enable hidden meue tab (young.vue)

--- frontend/src/components/Young.vue
+++ frontend/src/components/Young.vue
@@ -1,5 +1,16 @@
 <template lang="html">
   <div class="window_tile">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
+      <button class="w3-bar-item w3-button w3-large" onclick="w3_close()">Close &times;</button>
+      <a href="./young" class="w3-bar-item w3-button">Link to young</a>
+      <a href="./young2" class="w3-bar-item w3-button">Link to young2</a>
+      <a href="./yoon" class="w3-bar-item w3-button">Link to yoon</a>
+    </div>
+
+    <button id="openNav" class="w3-button w3-teal w3-xlarge" onclick="w3_open()">&#9776;</button>
+
     <div class="tile1">
     </div>
     <div class="tile2">
@@ -41,7 +52,21 @@
 
 <script>
 export default {
+/*
+  function w3_open() {
+    document.getElementById("window_tile").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+  },
+  function w3_close() {
+    document.getElementById("window_tile").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+  }
+*/
 }
+
 </script>
 
 <style scoped>
