a835756e6bde42cfc584c44a677f7051e3556607
t0dd <swjasontodd@gmail.com>
2018-04-01 12:06:38 +0900

    Reset components

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
-import Login from '@/components/Login'
-import Yoon from '@/components/Yoon'
-import Yoon2 from '@/components/Yoon2'
-import Young from '@/components/Young'
-import Young2 from '@/components/Young2'
-import Seo from '@/components/seo'
 Vue.use(Router)
 
 export default new Router({
@@ -16,36 +10,6 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/yoon',
-      name: 'Yoon',
-      component: Yoon
-    },
-    {
-      path: '/yoon2',
-      name: 'Yoon2',
-      component: Yoon2
-    },
-    {
-      path: '/young',
-      name: 'Young',
-      component: Young
-    },
-    {
-      path: '/young2',
-      name: 'Young2',
-      component: Young2
-    },
-    {
-      path: '/seo',
-      name: 'Seo',
-      component: Seo
     }
   ]
 })
