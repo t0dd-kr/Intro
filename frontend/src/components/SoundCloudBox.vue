30a6b2f5a0efab34f75126fe61ea64e9b7818dc9
DESKTOP-KLP1POH\yoon <ahnsh03@gmail.com>
2018-04-01 16:54:10 +0900

    Edit SoundCloud for save

---
+++ frontend/src/components/SoundCloudBox.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="soundcloudbox">
+    <iframe
+    src="https://soundcloud.com/revsnjps0ny2/sets/emhrwp38lool"
+    height="300"
+    width="300">
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .soundcloudbox iframe{
+    height: 300px;
+    width: 300px;
+
+
+  }
+</style>
