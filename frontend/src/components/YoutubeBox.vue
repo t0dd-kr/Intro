a9adb6fec58ee2b717a0e1cf4c4fcae91af39dc1
chanki <kgichan12@gmail.com>
2018-04-01 17:05:29 +0900

    edit YoutubeBox

--- frontend/src/components/YoutubeBox.vue
+++ frontend/src/components/YoutubeBox.vue
@@ -18,10 +18,9 @@ export default {
 <style scoped>
 <<<<<<< HEAD
   .youtubeDiv iframe{
-=======
-  .container-youtube-box {
->>>>>>> b6005d3256d26d2b4421276ec367d8f062fb5d05
-    width: 300px;
-    height: 300px;
+    .container-youtube-box {
+      width: 300px;
+      height: 300px;
+    }
   }
 </style>
