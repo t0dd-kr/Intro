3cdef4606f3e85d1065d00e6140c4f535068ae74
DESKTOP-KLP1POH\yoon <ahnsh03@gmail.com>
2018-04-01 17:11:19 +0900

    Edit SoundCloudBox edit Size

--- frontend/src/components/SoundCloudBox.vue
+++ frontend/src/components/SoundCloudBox.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div class="container-soundcloud-box">
     <iframe
-      width="100%"
-      height="450"
+      width="300px"
+      height="300px"
       scrolling="no"
       frameborder="no"
       src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/41395010&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true">
