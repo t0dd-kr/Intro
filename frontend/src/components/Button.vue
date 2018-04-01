f652183d4fd00223dad088d22cae7ddb1b474225
DESKTOP-KLP1POH\yoon <ahnsh03@gmail.com>
2018-04-01 15:06:47 +0900

    Edit Button Css change

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -11,8 +11,18 @@ export default {
 
 <style scoped>
   .button input {
-
+    width: 100%;
+    padding: 2rem;
+    font-size: 1.5rem;
+    background-color: #ccc;
+    border-radius: 0.5rem;
+    border: none;
   }
-  
+  .button input:active {
+    position: relative;
+    top: 2px;
+    background-color: #999;
+  }
+
 
 </style>
