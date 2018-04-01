f4c2e53ac11dacaa0364ebed89f66de65c9abd92
t0dd <swjasontodd@gmail.com>
2018-04-01 15:10:43 +0900

    Add ProfileImage component

---
+++ frontend/src/components/ProfileImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-profile-image">
+    <a href="javascript:loadImg();">
+      <div class="profile-image">
+        <img src="" alt="">
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ProfileImage',
+    data () {
+      return {
+      }
+    },
+    methods: {
+      loadImg: function() {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
