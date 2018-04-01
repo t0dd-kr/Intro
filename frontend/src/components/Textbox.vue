9998f886c8fc007238118796f03b92454f889a5b
chanki <kgichan12@gmail.com>
2018-04-01 14:48:04 +0900

    Textbox Commit

--- frontend/src/components/Textbox.vue
+++ frontend/src/components/Textbox.vue
@@ -10,9 +10,9 @@ export default {
 </script>
 
 <style scoped>
-  .textbox {
+  .textbox input{
     width : 100%;
     height : 1.5rem;
-    padding: 0.5rem 1.5rem 0.5rem 5rem;
+    padding-left: 0.5rem;
   }
 </style>
