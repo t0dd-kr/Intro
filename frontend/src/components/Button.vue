283131b5aee05b6f0a359ab4e82865bbbfefcd85
DESKTOP-KLP1POH\yoon <ahnsh03@gmail.com>
2018-04-01 13:56:20 +0900

    Add Buton.vue Create Button

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="Button">
+    <input type="button" value="login" v-on:click="login">
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .button {
+    width: 100%;
+    height: 1.5rem;
+    padding: .5rem 1.5rem .5rem 5rem;
+</style>
