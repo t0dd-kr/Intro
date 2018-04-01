a86a02c174f7e9de3fe14cd61709bc45f5507ba4
chanki <kgichan12@gmail.com>
2018-04-01 12:33:26 +0900

    Create Textbox

---
+++ frontend/src/components/Textbox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Textbox">
+      <input v-bind:input="textType" placeholder="입력하세요" ,>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
