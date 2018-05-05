f215ababcc0afac3aabe4ce5bb85818b2afcc477
chanki <kgichan12@gmail.com>
2018-05-05 22:37:07 +0900

    Make header file and add header into index, and add asset

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="header">
+    <div class="bind">
+
+      <div class="searchBox">
+        <input type="text" placeholder="검색하고 싶은 프로젝트를 검색해보세요~">
+        <img src="../assets/search.svg" onclick="">
+      </div>
+
+      <div class="name">
+        <img src="../assets/header_logo.png" alt="">
+      </div>
+
+        <div class="joinButton">
+          <a href="./login">로그인</a>
+          <a href="#" style="border-style: solid; border-width: 1px; padding : 5px 8px 3px 8px ;border-radius: 4px; margin-left: 10px ;">회원가입</a>
+        </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .header{
+    top : 0;
+    background-color: rgb(4,147,243);
+    width: 100%;
+    height : 60px;
+
+  }
+  .bind{
+    margin: auto;
+    width: 1200px;
+    text-align: center;
+    vertical-align: middle;
+  }
+  .bind .searchBox{
+    display: inline-flex;
+    float: left;
+    margin-left: 60px;
+    margin-top: 13px;
+  }
+  .bind .searchBox input{
+    height: 30px;
+    width: 200px;
+    padding-left: 8px;
+    border-style: none;
+    border-radius: 8px 0px 0px 8px;
+    outline-style: none;
+  }
+  .searchBox img{
+    width: 20px;
+    height: 32px;
+    padding-right: 8px;
+    background-color: white;
+    border-radius: 0px 8px 8px 0px;
+  }
+  .name{
+    display: inline-block;
+    margin-top: 5px;
+  }
+  .name img{
+    margin-top: 8px;
+  }
+
+
+  .joinButton{
+    float: right;
+    margin-top: 22px;
+    margin-right: 60px;
+    font-size: 16px;
+
+
+  }
+  .joinButton a{
+    text-decoration: none;
+    color: white;
+  }
+
+</style>
--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -1,14 +1,28 @@
 <template lang="html">
   <div class="index">
-
+    <div class="header">
+      <Header/>
+    </div>
   </div>
 
 </template>
 
 <script>
+import Header from './Header'
 export default {
+  name: 'Index',
+  data () {
+    return {
+    }
+  },
+  components: {
+    Header
+  }
 }
 </script>
 
 <style scoped>
+.header{
+  width: 100%;
+}
 </style>
