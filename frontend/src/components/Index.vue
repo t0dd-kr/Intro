a4ba50fbf34246d4f50366c408f80e1171b52a74
chanki <kgichan12@gmail.com>
2018-05-06 09:53:46 +0900

    add intro logo into index.vue

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -3,6 +3,12 @@
     <div class="header">
       <Header/>
     </div>
+    <div class="intro-main-logo">
+      <img src="../assets/main_logo.png" alt="메인로고">
+        <div class="main-text">
+          최고의 팀과 최고의 프로젝트를 시작하세요
+        </div>
+    </div>
   </div>
 
 </template>
@@ -25,4 +31,18 @@ export default {
 .header{
   width: 100%;
 }
+.intro-main-logo{
+  width: 100%;
+  background-color: rgb(241,247,251);
+  text-align: center;
+}
+.intro-main-logo img{
+  display: block;
+  margin: auto;
+  padding: 50px 0 30px 0;
+}
+.main-text{
+  padding: 0 0 40px 0 ;
+}
+
 </style>
